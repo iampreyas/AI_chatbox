03547ff58b980a433b65d9681d96dc148abb8918
iampreyas <preyasparmar5713@gmail.com>
2026-09-15 20:35:56 +0530

    Chatbox working

--- app.py
+++ app.py
@@ -3,52 +3,35 @@ from groq import Groq
 import os
 from dotenv import load_dotenv
 load_dotenv()
-st.set_page_config
-(
-	page_title="AI Chatbox",
-	layout="centered"
-)
-st.markdown
-("""
-	<style>
-	.stApp
-	{
-		max-width:800px;
-		margin:auto;
-	}
-	</style>
-""",unsafe_allow_html=True)
+st.set_page_config(page_title="AI Chatbox",layout="centered")
 st.title("AI Chatbox")
-st.caption("Your Personal AI")
+st.caption("AI Chatbox")
 if st.button("Clear Chat"):
-	st.session_state.message=[]
-	st.rerun()
-if len(st.session_state.get("message",[]))==0:
-	st.info("Hello! i'm your AI Assistant..")
+    st.session_state.messages = []
+    st.rerun()
 if "messages" not in st.session_state:
-	st.session_state.messages=[]
-for message in st.session_state.message:
-	with st.chat_message(message["role"]):
-		st.markdown(message["content"])
-if prompt:=st.chat_input("Message AI Chatbox.."):
-	st.session_state.message.append({"role":"user","content":prompt})
-	with st.chat_message("user"):
-		st.markdown(prompt)
-	with st.chat_message("assistant"):
-		with st.spinner("Thinking.."):
-			try:
-				client=Groq(api_key=os.getenv("GROQ_API_KEY"))
-				api_message=[{"role":m["role"], "content": m["content"]}
-				for m in st.session_state.messages]
-				response=client.chat.completion.create
-				(
-					model="llama-3.3-70b-versatile",
-					message=api_messages,
-					temperature=0.7,
-				)
-				reply=response.choices[0].message.content
-				st.markdown(reply)
-				st.session_state.message.append({"role":"assistant","content":reply})
-			expect Exception as e:
-				st.error(f"Error:{e}")
-				st.info("please check your GROQ_API_KEY in .env file")
+    st.session_state.messages = []
+if len(st.session_state.messages) == 0:
+    st.info(" Hello! i'm AI , Ask me")
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+if prompt := st.chat_input("Message AI Chatbox..."):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            try:
+                client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+                api_messages = [{"role": m["role"], "content": m["content"]} for m in st.session_state.messages]
+                response = client.chat.completions.create(
+                    model="openai/gpt-oss-20b",
+                    messages=api_messages,
+                    temperature=0.7,
+                )
+                reply = response.choices[0].message.content
+                st.markdown(reply)
+                st.session_state.messages.append({"role": "assistant", "content": reply})
+            except Exception as e:
+                st.error(f"Error: {e}")
